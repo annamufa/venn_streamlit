d0359801408aee6681252dd934233e7923f0ffe4
annamufa <38862917+annamufa@users.noreply.github.com>
2021-07-26 09:58:23 +0100

    Update venn_app.py

--- venn_app.py
+++ venn_app.py
@@ -3,7 +3,9 @@ from venn import venn, get_intersections
 import re
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
-st.title('Venn')
+st.title('Venn plots')
+
+st.text('You can choose the number of lists on the left, paste each list and label them. A venn diagram will be created with each update. You can then get the intersection for your lists')
 
 collect_list = lambda x: [i for i in re.split(",", x) if i != ""]
 
